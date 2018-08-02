f381fa9dd0365dd14b71c24397f586ac6a9eb3c6
alan cao <zongying.cao@dxc.com>
2018-08-02 10:48:48 -0400

    fix bugs in package
    Signed-off-by: alan cao <zongying.cao@dxc.com>

--- clear_files.sh
+++ clear_files.sh
@@ -2,3 +2,5 @@ find -name "*~" | xargs -r rm
 find -name "*.staging" | xargs -r rm
 find -name "*#" | xargs -r rm
 find ./nodejs_codegen -name "*.pyc" | xargs -r rm
+rm dist -rf
+rm nodejs_codegen.egg-info -rf
